9cf83baa43261e7411ebc48db6d4bc8997060dfc
Minghao Liu <mhliu0419@gmail.com>
2019-11-20 17:30:28 -0500

    Update tutorial.R

--- 3.KNN/tutorial.R
+++ 3.KNN/tutorial.R
@@ -1,1 +1,16 @@
+# KNN
+
+# Pros:
+#   Simple. Training is trivial. Works with any number of Classes
+#   Easy to add more data. Few parameters: K, Distance Metric
+
+# Cons:
+#   High prediction cost(worse for large data sets)
+#   Not good with high dimensional data
+#   Categorial fectures don't work well
+
+
+install.packages("ISLR")
+library(ISLR)
+str(Caravan)
 
