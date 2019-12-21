ba7d9560e0a0f05d608233071303dce8cf5d10a6
Minghao Liu <minghao.liu@columbia.edu>
2019-12-21 17:28:05 -0500

    Update tutorial.R

--- 2.Logistic_Regression/tutorial.R
+++ 2.Logistic_Regression/tutorial.R
@@ -1,4 +1,3 @@
-
 # Logistic Regression Learning
 # Titanic Data Set
 # To predict the survival on the Titanic
@@ -32,6 +31,64 @@ ggplot(df.train, aes(Age)) + geom_histogram(bins = 20, alpha = 0.5, fill = 'blue
 ggplot(df.train, aes(SibSp)) + geom_bar()
 
 ggplot(df.train, aes(Fare)) + geom_histogram(bins = 20, alpha = 0.5, fill = 'green', color = 'black')
+
+
+# To fill the missing data for age based on Pclass
+
+pl <- ggplot(df.train, aes(Pclass, Age))
+pl
+pl <- pl + geom_boxplot(aes(group = Pclass, fill = factor(Pclass) ,alpha = 0.4))
+pl
+pl + scale_y_continuous(breaks = seq(min(0), max(80), by = 2)) + theme_bw()
+
+
+# Imputation of  age based on class
+
+impute_age <- function(age,class){
+  out <- age
+  for (i in 1:length(age)){
+    
+    if (is.na(age[i])){
+      
+      if (class[i] == 1){
+        out[i] <- 37
+        
+      }else if (class[i] == 2){
+        out[i] <- 29
+        
+      }else{
+        out[i] <- 24
+      }
+    }else{
+      out[i]<-age[i]
+    }
+  }
+  return(out)
+}
+
+
+fixed.ages <- impute_age(df.train$Age, df.train$Pclass)
+
+
+df.train$Age <- fixed.ages
+
+
+missmap(df.train, main = 'Imputation Check', col = c('yellow', 'black'), legend = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
