1c910da000747dbd6713dcfbf595e5b43a342ad4
Minghao Liu <minghao.liu@columbia.edu>
2019-12-23 17:34:45 -0500

    Update code.R

--- 2.Logistic_Regression/code.R
+++ 2.Logistic_Regression/code.R
@@ -18,3 +18,30 @@ summary(adult)
 
 
 # Data Cleaning
+
+
+table(adult$type_employer)
+
+adult$type_employer <- sapply(adult$type_employer, as.character)
+
+typeof(adult$type_employer)
+
+length(adult$type_employer)
+
+for (i in 1:32561) {
+  
+  
+  if (adult$type_employer[i] == 'Never-worked' | adult$type_employer[i] == 'Without-pay') {
+    
+    adult$type_employer[i] <- 'Unemployed'
+    
+  }
+  
+  
+}
+
+
+
+
+
+
