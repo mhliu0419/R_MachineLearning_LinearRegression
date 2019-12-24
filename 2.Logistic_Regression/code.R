ce07a9bbb8ea58fe2006f82a846506b84a1c95fc
Minghao Liu <minghao.liu@columbia.edu>
2019-12-23 23:12:44 -0500

    Update code.R

--- 2.Logistic_Regression/code.R
+++ 2.Logistic_Regression/code.R
@@ -22,6 +22,8 @@ summary(adult)
 
 table(adult$type_employer)
 
+# Combine Never-worked and Without-pay into a single group called "Unemployed"
+
 adult$type_employer <- sapply(adult$type_employer, as.character)
 
 typeof(adult$type_employer)
@@ -41,7 +43,65 @@ for (i in 1:32561) {
 }
 
 
+# Combine State and Local gov jobs into a category called SL-gov
+
+for (i in 1:32561) {
+  
+  
+  if (adult$type_employer[i] == 'State-gov' | adult$type_employer[i] == 'Local-gov') {
+    
+    adult$type_employer[i] <- 'SL-gov'
+    
+  }
+  
+  
+}
+
+# Combine self-employed jobs into a category called self-emp.
+
+
+for (i in 1:32561) {
+  
+  
+  if (adult$type_employer[i] == 'Self-emp-inc' | adult$type_employer[i] == 'Self-emp-not-inc') {
+    
+    adult$type_employer[i] <- 'SL-gov'
+    
+  }
+  
+  
+}
 
 
 
+# Martial
 
+table(adult$marital)
+
+# Reduce this to three groups:
+# 1.Married
+# 2.Not-Married
+# 3.Never-Married
+
+
+
+group_marital <- function(mar){
+  mar <- as.character(mar)
+  
+  # Not-Married
+  if (mar=='Separated' | mar=='Divorced' | mar=='Widowed'){
+    return('Not-Married')
+    
+    # Never-Married   
+  }else if(mar=='Never-married'){
+    return(mar)
+    
+    #Married
+  }else{
+    return('Married')
+  }
+}
+
+adult$marital <- sapply(adult$marital,group_marital)
+
+table(adult$marital)
