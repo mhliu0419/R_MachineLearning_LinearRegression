d0457ba154b28cb7e24fda4f7283c68d65cc76ad
Minghao Liu <minghao.liu@columbia.edu>
2020-07-25 22:38:11 -0400

    Update tutorial.r

--- 4.Decision_Tree/tutorial.r
+++ 4.Decision_Tree/tutorial.r
@@ -1,1 +1,23 @@
+install.packages("rpart")
+library(rpart)
+help("rpart")
+## rpart allows to create Recursive Partitioning and Regression Trees
+
+## load data
+str(kyphosis)
+summary(kyphosis)
+
+
+## build a tree model
+
+tree <- rpart(Kyphosis ~ ., method = 'class', data = kyphosis)
+
+## display cp table
+printcp(tree)
+
+## plot decision tree
+plot(tree, uniform = T, main = 'Kyphosis Tree')
+
+## label the decision tree plot
+text(tree, use.n = T, all = T)
 
