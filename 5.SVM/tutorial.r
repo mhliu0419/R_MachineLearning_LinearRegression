0e875157cc1982e96f68660791e18b35a6429c80
Minghao Liu <minghao.liu@columbia.edu>
2020-07-27 03:10:15 -0400

    Update tutorial.r

--- 5.SVM/tutorial.r
+++ 5.SVM/tutorial.r
@@ -1,1 +1,21 @@
+# Load data
 
+library(ISLR)
+head(iris)
+
+
+# Install package of SVM
+
+#install.packages('e1071')
+library(e1071)
+help("svm")
+
+# Create a SVM model
+
+model <- svm(Species ~ ., data = iris)
+summary(model)
+
+# Predict/Classify with SVM model
+
+pred.values <- predict(model, iris[1:4])
+table(pred.values, iris$Species)
